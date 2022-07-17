8e10f49524c8f5d9d9d12f71fa617b9ad66bcf98
JustSong <quanpengsong@gmail.com>
2022-07-17 17:06:31 +0800

    Able to close now

--- main.py
+++ main.py
@@ -12,23 +12,36 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.gofile = None
 
     @pyqtSlot()
     def on_startBtn_clicked(self):
-        if os.path.exists("go-file.exe"):
-            port = self.portSpinBox.text()
-            host = self.hostLineEdit.text()
-            path = self.pathLineEdit.text()
-            subprocess.Popen(["go-file.exe", "--port", f"{port}", "--host", f"{host}", "--path", f"{path}"])
-            self.statusbar.showMessage("服务已启动")
-            self.startBtn.setEnabled(False)
+        if self.gofile is None:
+            if os.path.exists("go-file.exe"):
+                port = self.portSpinBox.text()
+                host = self.hostLineEdit.text()
+                path = self.pathLineEdit.text()
+                self.gofile = subprocess.Popen(["go-file.exe", "--port", f"{port}", "--host", f"{host}", "--path", f"{path}"], shell=True)
+                self.statusbar.showMessage("服务已启动")
+                self.startBtn.setText("终止")
+            else:
+                QMessageBox.information(self, "未能找到 go-file.exe", "请点击更新按钮进行下载或者手动下载后放到本启动器相同目录下", QMessageBox.Ok)
         else:
-            QMessageBox.information(self, "未能找到 go-file.exe", "请点击更新按钮进行下载或者手动下载后放到本启动器相同目录下", QMessageBox.Ok)
+            if os.name == "nt":
+                subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=self.gofile.pid))
+            else:
+                # Not tested.
+                # https://stackoverflow.com/questions/4789837/how-to-terminate-a-python-subprocess-launched-with-shell-true
+                self.gofile.kill()
+            self.gofile = None
+            self.statusbar.showMessage("服务已终止")
+            self.startBtn.setText("启动")
 
     @pyqtSlot()
     def on_chooseBtn_clicked(self):
         path = QFileDialog.getExistingDirectory(self, "选择要分享的目录", ".")
         self.pathLineEdit.setText(path)
+        self.statusbar.showMessage(f"选择：{path}")
 
     @pyqtSlot()
     def on_updateBtn_clicked(self):
